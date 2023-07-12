a5fe68d1ea96e6a96f538576e6e3841a63adceed
Derovse <sashamartunyk2004@gmail.com>
2023-07-12 22:15:57 +0300

    d
    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import user from '../../data/user.json';
-import Profile from '../Profile/Profile';
-import Statistics from '../Statistics/Statistics';
-import data from '../../data/data.json';
-import friends from '../../data/friends.json';
-import FriendList from '../Friends/FriendList';
-import TransactionHistory from '../Transactions/TransactionHistory';
-import transactions from '../../data/transactions.json';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import Profile from '../components/Profile/Profile';
+import Statistics from '../components/Statistics/Statictics';
+import FriendList from '../components/Friends/Friendlist';
+import TransactionHistory from '../components/Transactions/Transactions';
 
 export const App = () => {
   return (
