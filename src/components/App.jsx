392a01558fcc250d77e50e119fca5552983b9d58
Derovse <sashamartunyk2004@gmail.com>
2023-07-12 22:11:32 +0300

    d
    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import user from '../data/user.json';
-import Profile from '../Profile/Profile';
-import Statistics from '../Statistics/Statictics';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+import Profile from '../Profile/Profile';
+import Statistics from '../Statistics/Statictics';
 import FriendList from '../Friends/Friendlist';
 import TransactionHistory from '../Transactions/Transactions';
-import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
