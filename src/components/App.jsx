545e3b3a7f9df7b4e15c915bd874d0d8658f0276
Derovse <sashamartunyk2004@gmail.com>
2023-07-12 18:31:29 +0300

    а
    а

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import React from 'react';
+import user from '../components/data/user';
+import Profile from './profile/profile';
+import Statistics from '../components/statistics/statictics'; // Укажите правильный путь к компоненту Statistics
+import data from '../components/data/data';
+import friends from '../components/data/friends.json';
+import FriendList from '../components/friends/friendlist'; // Укажите правильный путь к компоненту FriendList
+import TransactionHistory from '../components/transactions/transactions'; // Укажите правильный путь к компоненту TransactionHistory
+import transactions from '../components/data/transactions.json'; // Укажите правильный путь к файлу transactions.json
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/friends/friendlist.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../friends/friends.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles['friend-list']}>
+      {friends.map(friend => (
+        <li className={styles.item} key={friend.id}>
+          <span
+            className={`${styles.status} ${
+              friend.isOnline ? styles.online : styles.offline
+            }`}
+          ></span>
+          <img
+            className={styles.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={styles.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
