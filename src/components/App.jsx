1e68841ab269ee7eef1b0532fa82a8203d144e73
Derovse <sashamartunyk2004@gmail.com>
2023-07-12 21:39:40 +0300

    а

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import user from '../components/data/user';
-import Profile from './profile/profile';
-import Statistics from '../components/statistics/statictics'; // Укажите правильный путь к компоненту Statistics
+import Profile from './profile/Profile';
+import Statistics from './statistics/Statictics';
 import data from '../components/data/data';
 import friends from '../components/data/friends.json';
-import FriendList from '../components/friends/friendlist'; // Укажите правильный путь к компоненту FriendList
-import TransactionHistory from '../components/transactions/transactions'; // Укажите правильный путь к компоненту TransactionHistory
-import transactions from '../components/data/transactions.json'; // Укажите правильный путь к файлу transactions.json
+import FriendList from './friends/Friendlist';
+import TransactionHistory from './transactions/Transactions';
+import transactions from '../components/data/transactions.json';
 
 export const App = () => {
   return (
